914e8a6e0b1b0ea3d345626d3421d58ce657d12e
xiaochengguo <1272686962@qq.com>
2020-01-08 00:18:53 +0800

    express ok

--- express/express01/app.js
+++ express/express01/app.js
@@ -1,7 +1,7 @@
 const express =require('./express')
 const app = express()
 app.get('/',(req, res)=> {
-    res.json("hello, world!");
+    res.end("hello, world!");
 })
 
 app.listen(1699,()=> {
--- express/express01/express.js
+++ express/express01/express.js
@@ -18,18 +18,18 @@ const createApplication = () => {
                 handler
             })
         },
-        listen() {
-            const server = http.createServer((req, res, ...args)=> {
-                for(let i = i; i < router.length; i++) {
+        listen(...params) {
+            const server = http.createServer((req, res)=> {
+                for(let i = 1; i < router.length; i++) {
                     const {path, menthod, handler} = router[i] 
-                    const requerstUrl = url.parse(req, true).toLowerCase()
+                    const requerstUrl = url.parse(req.url, true).pathname.toLowerCase()
                     if(path === requerstUrl) {
                         return handler(req, res)
                     }
                 }
                 router[0].handler(req, res)
             })
-            server.listen.apply(server, [req, res, ...args])
+            server.listen.apply(server, params)
         }
     }
 }
